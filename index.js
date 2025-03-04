de1868148d02f22f8f45921aeddf07e66a2525ea
Brian <brian.kiprono8@student.moringaschool.com>
2025-03-04 15:04:57 +0300

    learning strings

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser ='Moringa'
+
+const welcomeMessage = `'Welcome to Flatbook, ';${currentUser}!`;
+
+//changing text to uppercase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// displaying "welcome" and including the letter M from currentUser
+//using slice
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
